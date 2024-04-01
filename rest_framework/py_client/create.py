1ccab33d278d20084d663adc68f40b44cacb6e8b
MD.RAYHAN KHAN <98764238+rayhankhan2192@users.noreply.github.com>
2024-04-02 01:28:46 +0600

    Add files via upload

--- rest_framework/py_client/create.py
+++ rest_framework/py_client/create.py
@@ -2,14 +2,15 @@ import requests
 
 endpoint = "http://localhost:8000/api/product/create/"
 
+headers = {'Authorization': 'Token 84ca2ee7a1f7e5c503ca17a6a858dc18ac381db0'}
 
 data = {
     'title': 'juice',
-    'content': 'Mango',
-    'price': 200
+    'content': 'Strawbery',
+    'price': 300
 }
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 
 #print(get_response.text)
 
--- rest_framework/py_client/detail.py
+++ rest_framework/py_client/detail.py
@@ -1,13 +1,26 @@
 import requests
-
+from getpass import getpass
 #endpoint = "http://localhost:8000/api/"
-endpoint = "http://localhost:8000/api/product/view/123" #http://127.0.0.1:8000/
 
-get_response = requests.get(endpoint)
+auth_endpoint = "http://localhost:8000/api/auth/"
 
-#print(get_response.text)
+username = input("username: ")
+password = getpass("Password: ")
 
-print(get_response.json())
-print(get_response.status_code)
+auth_response = requests.post(auth_endpoint,
+                              json={'username': username, 'password': password})
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Token {token}"
+    }
+    
+    endpoint = "http://localhost:8000/api/product/create/" #http://127.0.0.1:8000/
+
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
+    print(get_response.status_code)
 
 
